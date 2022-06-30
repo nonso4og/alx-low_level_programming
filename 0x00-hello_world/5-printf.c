2be59c297562ffa13395fc32df3dcf703823ee7e
root <root@DESKTOP-0CVDECS.localdomain>
2022-06-30 20:28:24 +0100

    task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Return: Always 0 (Success)
+ *  Return: Always 0 (Success)
  */
  int main(void)
  {
- printf("with proper grammar, but the outcome is a piece of art,\n");
+ char a;
+ int i;
+ long int li;
+ long long int lli;
+ float f;
+ printf("Size of a char: &lu byte(s) \n", (unsigned long)sizeof(a));
+ printf("size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
+ printf("size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
+ printf("size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+ printf("size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
  return (0);
  }
