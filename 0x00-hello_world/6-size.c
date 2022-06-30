2bae62dabda6504282b44e09305364b60bda9f5c
nonso iwedinobi <Nonso Iwedinobi>
2022-06-30 13:16:46 -0700

    task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,16 +1,11 @@
-#include <stdio.h>
-<<<<<<< HEAD
+ #include <stdio.h>
+ /**
+ *main - Entry point
+ * Return: Always 0 (Success)
+ */
+ int main(void)
+ {
 
-=======
-/**
- *
- *  *main - Entry point
- *
- *   * Return: Always 0 (Success)
- *
- *    */
-int main(void)
-{
 	char a;
 	int i;
 	long int li;
@@ -19,8 +14,8 @@ int main(void)
 	printf("Size of a char: %lu byte(s) \n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(li));
-	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(lli));
+	printf("Size of a long long int: %lu byte(s)\n", (unsigned long)sizeof(l
 	printf("Size of a float: %lu byte(s)\n", (unsigned long)sizeof(f));
+
 	return (0);
 }
->>>>>>> 8bc1628d382fd3e358901d68c6d17094c004589c
