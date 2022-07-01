ff66a91b6a296c586e929e5086773ef75c43bb34
root <root@DESKTOP-0CVDECS.localdomain>
2022-07-01 21:48:24 +0100

    Task 1

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
 /**
  * main - Entry point
  *
@@ -21,11 +20,11 @@ int main(void)
 
 
 	if (L > 5)
-		printf("%s %d is %d and is greater than 5\n", str, n, L);
+	printf("%s %d is %d and is greater than 5\n", str, n, L);
 	else if (L == 0)
-		printf("%s %d is %d and is 0\n", str, n, L);
+	printf("%s %d is %d and is 0\n", str, n, L);
 	else if (L < 6)
-		printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
+	printf("%s %d is %d and is less than 6 and not 0\n", str, n, L);
 		
 
 	return (0);
